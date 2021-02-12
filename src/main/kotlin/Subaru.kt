5067e94f7211222b30b884e0168ae55e9b87eb5c
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-02-12 17:42:34 +0100

    cd..

---
+++ src/main/kotlin/Subaru.kt
@@ -0,0 +1,17 @@
+class Subaru(override val silnik: Silnik, override val skrzyniaBiegow: SkrzyniaBiegow) : Samochod(){
+    override fun jedz() {
+        TODO("Not yet implemented")
+    }
+
+    override fun nacisnijHamulec() {
+        TODO("Not yet implemented")
+    }
+
+    override fun skrec() {
+        TODO("Not yet implemented")
+    }
+
+    override fun zaciagnijHamulecReczny() {
+        TODO("Not yet implemented")
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,17 @@
 fun main() {
-    val samochod1 = Passat(Silnik("Benzyna",150),SkrzyniaBiegow("Manualna"))
+    val samochod1 = Passat(Silnik("Benzyna", 150), SkrzyniaBiegow("Manualna"))
     samochod1.jedz()
     samochod1.nacisnijHamulec()
     samochod1.info()
     samochod1.skrec()
 
-    val samochod2 = Bmw(Silnik("Benzyna",260),SkrzyniaBiegow("DMG"))
+    val samochod2 = Bmw(Silnik("Benzyna", 260), SkrzyniaBiegow("DMG"))
     samochod2.jedz()
     samochod2.nacisnijHamulec()
     samochod2.skrec()
     samochod2.info()
+
+    val samochod3 = Subaru(Silnik("Benzyna", 350), SkrzyniaBiegow("Sekwencyjna"))
+
+    samochod3.info()
 }
